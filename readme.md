33366932f5dafe46cf03cb0e68abebfab7cf0971
itachixa <xaodin12@gmail.comas5921>
2025-02-20 19:25:49 +0100

    i initiaze readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+### My solution for OOP LAB in C++ 
+ <img src="img/image.png" alt="messager screem ">
+ 
+- **Description**:IN this reposotory i push my solution for lab 
--- real/operator.cpp
+++ real/operator.cpp
@@ -2,14 +2,35 @@
 using namespace std;
 
 int main() {
-    int x = 5;
-    int y = ++x; // x is incremented first, then assigned to y
-    cout << "x = " << x << ", y = " << y << endl; // Output: x = 6, y = 6
+    // int x = 5;
+    // int y = ++x; // x is incremented first, then assigned to y
+    // cout << "x = " << x << ", y = " << y << endl; // Output: x = 6, y = 6
 
+    
+     int x = 5;
+     x+=1;
+     int y=x;
+     cout << "x = " << x << ", y = " << y << endl; // Output: x = 6, y = 6
 
+    
+  
 
+    
+    // x=5;
+    // int z = x++; // x is assigned to y first (z = 5), then x is incremented
+    // cout << "x = " << x << ", z = " << z << endl; // Output: x = 6,  z= 5
 
-    int z = x++; // x is assigned to y first (z = 5), then x is incremented
+    
+    x=5;
+    int z= x;
+    x+=1;
     cout << "x = " << x << ", z = " << z << endl; // Output: x = 6,  z= 5
+
+
+    // warrming don't confuse the += is juste  the additional and affectation operator 
+    // so  x+=1 equal x=x+1  
+    //  and x=+1 is just affectation  equal x=1 
+
+    
     return 0;
 }
